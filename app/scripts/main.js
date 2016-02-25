9ef5c296943fd045488406d14db4b1ae57962cd0
Sule Stone <sulestone@att.net>
2016-02-24 20:46:48 -0500

    add images and css styleing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('\'Allo \'Allo!');
+// Closes the Responsive Menu on Menu Item Click
+$('.overlay-menu ul li a').click(function() {
+    $('.button_container:visible').click();
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Navigation show/hide
+$('.toggle').click(function() {
+    if ($('#overlay.open')) {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+    }
+});
+
+// jQuery Parallax. More info here-> https://github.com/IanLunn/jQuery-Parallax
+$(function(){
+    // apply parallax effect only when body has the ".parallax-page" class
+    if ($('body').hasClass('parallax-page')) {
+        //.parallax(xPosition, speedFactor, outerHeight) options:
+        //xPosition - Horizontal position of the element
+        //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+        //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+        
+        $('#parallax-slide').parallax("50%", 0.1);
+        $('#products').parallax("50%", 0.1);
+        $('#portfolio').parallax("50%", 0.1);
+        $('#page-aboutus').parallax("50%", 0.1);
+    }
+});
